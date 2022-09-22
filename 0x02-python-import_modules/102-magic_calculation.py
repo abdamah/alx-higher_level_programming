672e8e0101bb52f25c2a5a59be8a9100d0e49445
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-09-22 19:36:56 +0000

    Add magic_calculation.

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """Bytecode for python3"""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return(sub(a, b))

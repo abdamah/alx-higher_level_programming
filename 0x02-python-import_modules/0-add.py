d00b5cb942f97cc52cdced591128304496eea0b9
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-09-22 18:57:40 +0000

    Fix: add function.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    # importing add function from add_0 file or module.
+if __name__ == "__main__":    
     from add_0 import add
 
     a = 1
     b = 2
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))

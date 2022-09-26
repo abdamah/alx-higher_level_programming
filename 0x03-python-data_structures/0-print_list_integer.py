8d85f6b3bf45775678d6e4432bd5bc6285eb49c1
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-09-26 18:10:23 +0000

    Use format string func.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     """Displays all integers of a list"""
     for i in range(len(my_list)):
-        print(f'{my_list[i]}')
+        print('{:d}'.format(my_list[i])

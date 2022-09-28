c60a765b33b60be783b719b80bc4cc84ea7adb12
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-09-28 19:31:05 +0000

    Fix: format string.

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     """prints a dictionary by ordered keys."""
     for key in sorted(a_dictionary.keys()):
-        print(f"{key}: {a_dictionary[key])}")
+        print(f"{key}: {a_dictionary[key]}")

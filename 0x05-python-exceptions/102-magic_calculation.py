84cbd77a87e6f2285834f250a33c189ee7816e38
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-10 19:50:29 +0000

    Fix: exection message.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+     """Magic function with try-catch block."""
     result = 0
+
     for i in range(1, 3):
         try:
             if (i > a):
-                raise Exception("Too far")
+                raise Exception('Too far')
             else:
-                result += (a ** b) / i
+                result += a ** b / i
         except:
-            result = b + a
-            break
+            result = a + b
+            pass
     return (result)

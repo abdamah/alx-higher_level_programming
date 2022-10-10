1183bd9fc13bed10776806d30c5429615c3486f6
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-10 18:45:37 +0000

    Fix: add TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     """prints an integer with "{:d}".format()"""
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (TypeError, ValueError):
         return (False)
 
     return (True)

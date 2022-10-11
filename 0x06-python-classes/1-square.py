cdac407e9fdf6c33988297774d0f23f6c986abc8
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-11 19:11:16 +0000

    Include size attr in Square class.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Working with python OOPS."""
+"""
+New class Square
+"""
 
 
 class Square:
-    """ Define Square classs with size attribute"""
+    """ Defines a Square """
     def __init__(self, size):
         self.__size = size
-

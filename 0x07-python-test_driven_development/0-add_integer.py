b3fa6fc8075764d0960a4112ab0f30b41a1eeb95
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-18 17:46:09 +0000

    Initial commit.

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+i#!/usr/bin/python3
+"""Module containing an add function for testing"""
+
+
+def add_integer(a, b=98):
+    """ adds integers
+        Arguments:
+        @a: first integer
+        @b: second integer, default to 98 if not given
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)

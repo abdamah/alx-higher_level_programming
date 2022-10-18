8d269512ee6ea03310a0730dae87be82a07fee59
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-18 18:46:08 +0000

    Fix: comment.

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "BestSchool" + ", BestSchool" * count[0]

3132d99750fe8452c11ba25b8fe20ddfd0f125b9
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-10-24 16:33:09 +0000

    Add MyInt class that inherits int class and inverted eq and ne magics.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 """Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
-
+    """ My int inherits from int """
     def __eq__(self, value):
-        """Override == opeartor with != behavior."""
-        return self.real != value
+        """ Function for equals """
+        return(int(self) != int(value))
 
     def __ne__(self, value):
-        """Override != operator with == behavior."""
-        return self.real == value
-
+        """ Function for not equals """
+        return (int(self) == int(value))

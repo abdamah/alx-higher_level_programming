41db8ddccc030cda86912ee2e5cf07b87ed3485f
Abdallah Mahmoud <abdamoh1@gmail.com>
2022-12-22 18:02:02 +0000

    Fix bug for relatiships.

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""adds the State object “California”
-with the City “San Francisco”
-to the database hbtn_0e_100_usa"""
+"""
+creates the State "California" with the City "San Francisco" from the
+database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+"""
+
+import sys
+from unicodedata import name
+from venv import create
+from sqlalchemy import create_engine, true
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import Base, City
 
 if __name__ == "__main__":
-
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    session = Session(engine)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    session.add_all([new, new_city])
+    session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
     session.close()
-
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, text
+"""
+Defines a state model that contain the class definition
+ of a State and an instance Base = declarative_base()
+"""
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-"""
-    Module that performs creates a States class based off of Base.
-"""
-
+from relationship_city import Base, City
 Base = declarative_base()
 
 
 class State(Base):
     """
-        The ``States`` class which inherits from ``Base`` class.
+    inherits from Base Tips
+    links to the MySQL table states
+    class attribute id that represents a column
+     of an auto-generated, unique integer, can't
+      be null and is a primary key
+    class attribute name that represents a column
+     of a string with maximum 128 characters and
+      can't be null
     """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
